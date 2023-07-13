321c7eac5c7f4cb02a7bd664dd8f94c07c61d63d
BanKokSik <bankok2bankok2@gmail.com>
2023-07-13 12:00:44 +0300

    Added mvp

---
+++ BabyTracker/MVP/CreateProfile/Presenter/CreateProfilePresenter.swift
@@ -0,0 +1,19 @@
+//
+//  CreateProfilePresenter.swift
+//  BabyTracker
+//
+//  Created by Nikita Chekmarev on 11.07.2023.
+//
+
+import UIKit
+protocol CreateProfilePresenterDelegate: AnyObject{
+    
+}
+
+class CreateProfilePresenter: UIViewController {
+  
+    
+   
+    
+}
+
---
+++ BabyTracker/MVP/Registration/Presenter/RegistrationPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  RegistrationPresenter.swift
+//  BabyTracker
+//
+//  Created by Nikita Chekmarev on 13.07.2023.
+//
+
+import Foundation
+
+class RegistrationPresenter{
+    
+}
